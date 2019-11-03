0a725afc6832ed31cac7a6f6ae8835925ba38815
Joseph Weissman <jweissman1986@gmail.com>
2019-11-03 18:31:04 -0500

    Updates

---
+++ precache-manifest.f618e1797ba280121978cc8fb9fe764f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ed49f62d77b42de0da40ebbd25090a",
+    "url": "/figure/index.html"
+  },
+  {
+    "revision": "39a3449b0e856961d3a3",
+    "url": "/figure/static/css/main.86d35db4.chunk.css"
+  },
+  {
+    "revision": "6c2fcf19c9537843cfa6",
+    "url": "/figure/static/js/2.ed60697e.chunk.js"
+  },
+  {
+    "revision": "39a3449b0e856961d3a3",
+    "url": "/figure/static/js/main.5868f38d.chunk.js"
+  },
+  {
+    "revision": "0d3d5b399fc561b4ab01",
+    "url": "/figure/static/js/runtime-main.6aeabe5c.js"
+  }
+]);
